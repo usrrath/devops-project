cb58c7ccb6c1b1bd02b19982f28e5efc9300d286
b7pm_14pm <rath@UngSereyRath.local>
2026-03-18 15:44:16 +0700

    feat: add divides function

--- src/sum.js
+++ src/sum.js
@@ -9,4 +9,12 @@ function minus(a, b) {
 function multiply(a, b) {
   return a * b;
 }
-module.exports = { sum, minus, multiply };
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+module.exports = { sum, minus, multiply, divide };
